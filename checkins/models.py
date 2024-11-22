ff93bd7f771cbf41ae428d56b05ae09925dcc399
Nicknjano <nnjano254@gmail.com>
2024-11-22 11:57:15 +0300

    models created

--- checkins/models.py
+++ checkins/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class CheckIn(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # 1 = very poor, 5 = excellent
+    sleep_hours = models.FloatField()
+    stress_level = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    notes = models.TextField(blank=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Resource(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField()
+    category = models.CharField(max_length=50)  # e.g., 'Stress', 'Anxiety', etc.
+
+    def __str__(self):
+        return self.title
